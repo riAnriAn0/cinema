dfe29d2e94d554e6089257bac9866e905164f679
riAnriAn0 <156357069+riAnriAn0@users.noreply.github.com>
2025-07-18 14:38:51 -0300

    atualizacao

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
-import cinema_config.Sala;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Sala sala1 = new Sala(1, "Filme A");
-        sala1.showAssentos();
+       
     }
 }
